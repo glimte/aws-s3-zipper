072fa03cf910e2555776feb0f6c72c9220ceae0f
Brad Fowler <bradley.fowler123@gmail.com>
2019-08-06 12:31:25 +0100

    When zipping entire bucket zip file is not but into root

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ S3Zipper.prototype = {
         var t = this;
         params.zipFileName = '__' + Date.now() + '.zip';
 
-        if (params.s3ZipFileName.indexOf('/') < 0)
+        if (params.s3ZipFileName.indexOf('/') < 0 && params.s3FolderName)
             params.s3ZipFileName = params.s3FolderName + "/" + params.s3ZipFileName;
 
 
@@ -347,7 +347,7 @@ S3Zipper.prototype = {
         ///local file
         params.zipFileName = '__' + Date.now() + '.zip';
 
-        if (params.s3ZipFileName.indexOf('/') < 0)
+        if (params.s3ZipFileName.indexOf('/') < 0 && params.s3FolderName)
             params.s3ZipFileName = params.s3FolderName + "/" + params.s3ZipFileName;
 
         var finalResult;
