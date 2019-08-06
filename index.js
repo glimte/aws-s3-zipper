525c4ad2ae5696c56d8b6f565d62e0078c9111ae
Brad Fowler <bradley.fowler123@gmail.com>
2019-08-06 11:06:08 +0100

    removed need for AWS credentials. Resolves #32

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ var s3 = require('s3');
 function S3Zipper(awsConfig) {
     var self = this
     assert.ok(awsConfig, 'AWS S3 options must be defined.');
-    assert.notEqual(awsConfig.accessKeyId, undefined, 'Requires S3 AWS Key.');
-    assert.notEqual(awsConfig.secretAccessKey, undefined, 'Requires S3 AWS Secret');
     assert.notEqual(awsConfig.region, undefined, 'Requires AWS S3 region.');
     assert.notEqual(awsConfig.bucket, undefined, 'Requires AWS S3 bucket.');
 
